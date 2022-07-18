1908d3e8ca622eda1d0b059b37ea941406f89ead
zhangting5 <zhangting5@xiaomi.xom>
2022-07-18 19:39:39 +0800

    cargo build

---
+++ hello_cargo/target/debug/deps/hello_cargo-6ceb762b89beb231.d
@@ -0,0 +1,5 @@
+/Users/zhangting5/Documents/learn/rust-demo/hello_cargo/target/debug/deps/hello_cargo-6ceb762b89beb231: src/main.rs
+
+/Users/zhangting5/Documents/learn/rust-demo/hello_cargo/target/debug/deps/hello_cargo-6ceb762b89beb231.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/zhangting5/Documents/learn/rust-demo/hello_cargo/target/debug/hello_cargo: /Users/zhangting5/Documents/learn/rust-demo/hello_cargo/src/main.rs
